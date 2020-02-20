9430764509514f657ed5a4b1f38a997b88c0cd8b
ariara1998 <ariara1998@gmail.com>
2020-02-19 21:03:30 -0300

    resuelto

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,8 +4,28 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
-	var respuesta='si';
+	var numero;
+	var respuesta;
+
+	do{
+		numero = parseInt(prompt("Ingrese un numero"));
+		while(isNaN(numero)){
+			numero = parseInt(prompt("Eso no es un numero. Ingrese un numero"));
+		}
+		if(numero > 0){
+			positivo += numero;
+		}
+		else{
+			negativo *= numero;
+			contador++;
+		}
+		respuesta = prompt("Seguir acumulando");
+
+	}while(respuesta == 'si');
+
+	if(contador == 0){
+		negativo = 0;
+	}
 
 
 document.getElementById('suma').value=positivo;
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,17 +2,28 @@ function mostrar()
 {
 
 	var contador=0;
+	var numero;
+	var nummax =0;
+	var nummin =0;
+	var respuesta;
 	// declarar variables
+
+	do{
+		numero = parseInt(prompt("Ingrese un numero"));
+		while(isNaN(numero)){
+			numero = parseInt(prompt("Error. Ingrese un numero"));
+		}if(numero > nummax){
+			nummax = numero;
+		}
+		if(){
+			nummin = numero;
+		}
+		respuesta = prompt("Seguir contando");
 	
-	var respuesta='si';
+	}while(respuesta == 'si');
 
-	while(respuesta!='no')
-	{
-		
-	
-	}
-
-
+	document.getElementById("maximo").value = nummax;
+	document.getElementById("minimo").value = nummin;
 
 
 }//FIN DE LA FUNCIÓN
