8ec197630ca50554db14b3ef141ed1a5c5ee1180
ariara1998 <ariara1998@gmail.com>
2020-02-26 13:56:12 -0300

    resuelto

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -9,6 +9,8 @@ function mostrar()
 	var cantnegativo=0;
 	var cantcero=0;
 	var cantpares=0;
+	var promediopositvo;
+	var promedionegativo;
 	var respuesta="si";
 	//declarar contadores y variables 
 
@@ -17,6 +19,9 @@ function mostrar()
 		numero=parseInt(prompt("Ingrese un mumero"));
 		while(isNaN(numero)){
 			numero=parseInt(prompt("Error. Ingrese numero"));
+		}
+		if(numero%2==0){
+			cantpares++;
 		}
 		if(numero>0){
 			positivo+=numero;
@@ -29,18 +34,20 @@ function mostrar()
 			negativo+=numero;
 			cantnegativo++;
 		}
-		if(numero%2==0){
-			cantpares++;
-		}
 		respuesta=prompt("Quiere ingresar otro numero ?");
 	
 	}while(respuesta=="si");
+
+	promediopositvo=positivo/cantpositovo;
+	promedionegativo=negativo/cantnegativo;
 
 	document.write("Suma de negativos: "+negativo+"</br>");
 	document.write("Suma de positivos:"+positivo+"</br>");
 	document.write("Cantidad de negativos:"+cantnegativo+"</br>");
 	document.write("Cantidad de positivos:"+cantpositovo+"</br>");
 	document.write("canridad de ceros:"+cantcero+"</br>");
+	document.write("Preomedio de los negativos:"+promedionegativo+"</br>");
+	document.write("Promedio de los positivos:"+promediopositvo+"</br>");
 
 
 }//FIN DE LA FUNCIÓN
